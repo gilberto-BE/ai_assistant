e0480714faffb1a68761206a05bcb9d58315cb56
gilberto-BE <g.batres.estrada@gmail.com>
2024-05-22 18:26:34 +0200

    update with llama3

---
+++ src/local_llm_autogen.py
@@ -0,0 +1,14 @@
+local_llm_config = {
+    "config_list": [
+        {
+            "model": "NotRequired",
+            "api_key": "NotRequired",
+            "base_url": "http://0.0.0.0:4000",
+        }
+    ],
+    "cached_seed": None,
+}
+
+local_assistant = ConversableAgent("agent", llm_config=local_llm_config)
+user_proxy = UserProxyAgent("user", code_execution_config=False)
+assistant.initiate_chat(user_proxy, message="How can I help you today?")
