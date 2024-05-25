a276fb9709a97f322864d2723234e678ae190088
gilberto-BE <g.batres.estrada@gmail.com>
2024-05-25 19:20:13 +0200

    work with openai streamlit

--- src/streamlit_ui.py
+++ src/streamlit_ui.py
@@ -1,22 +1,44 @@
 import streamlit as st
+import time
+import random
+
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+            "I don't have real time access to data. I am only an AI",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
 
 
 def main():
-    st.title("Chatbot")
+    st.title("El Chato")
 
-    if 'messages' not in st.session_state:
+    if "messages" not in st.session_state:
         st.session_state.messages = []
 
     for message in st.session_state.messages:
-        with st.chat_message(message['role']):
-            st.markdown(message['content'])
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
     if prompt := st.chat_input("How are you today?"):
 
         with st.chat_message("User"):
             st.markdown(prompt)
-        st.session_state.messages.append({'role': "User", 'content': prompt})
+        st.session_state.messages.append({"role": "User", "content": prompt})
 
+    response = f"El chato: {prompt}"
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+        # st.markdown(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 if __name__ == "__main__":
