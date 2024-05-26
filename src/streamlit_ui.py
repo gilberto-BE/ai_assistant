e01aefabb6bd6c6dfdd07b9a261e8892e5201fe3
gilberto-BE <g.batres.estrada@gmail.com>
2024-05-26 11:23:51 +0200

    update assistant with gpt

--- src/streamlit_ui.py
+++ src/streamlit_ui.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import time
 import random
+from openai import OpenAI
+import os
 
 
 # Streamed response emulator
@@ -19,7 +21,12 @@ def response_generator():
 
 
 def main():
-    st.title("El Chato")
+    st.title("El Chato with GPT-4o")
+
+    client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
@@ -28,17 +35,22 @@ def main():
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    if prompt := st.chat_input("How are you today?"):
-
-        with st.chat_message("User"):
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
             st.markdown(prompt)
-        st.session_state.messages.append({"role": "User", "content": prompt})
 
-    response = f"El chato: {prompt}"
-    with st.chat_message("assistant"):
-        response = st.write_stream(response_generator())
-        # st.markdown(response)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        with st.chat_message("assistant"):
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 if __name__ == "__main__":
